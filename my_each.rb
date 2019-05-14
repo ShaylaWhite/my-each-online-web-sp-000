fbff2f5fc3174f5d8d1772487523c08ad3a49e1a
Shayla White <shayladwhite1@gmail.com>
2019-05-14 04:57:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(words)
-  words = []
   i = 0
-    while i < words.length 
-          yield(words[i])
+ 
+  while i < array.length
+    yield(words[i])
     i = i + 1
   end
  
- my_each
+  array
 end
 
 
